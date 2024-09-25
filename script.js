9182b05aaa54e0f9f55bebfca1f8fee22e9a745d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2024-09-25 14:56:25 -0300

    Código do exercício 1 - Erik Kaiyu Suguiyama

---
+++ script.js
@@ -0,0 +1,9 @@
+// Exercício 1
+
+// var variavelNulo = null; //Variável do tipo nulo.
+// var variavelUndefined; //Variável do tipo undefined.
+// var variavel = 10; //Variável com valor
+
+// console.log(variavelNulo);
+// console.log(variavelUndefined);
+// console.log(variavel);
