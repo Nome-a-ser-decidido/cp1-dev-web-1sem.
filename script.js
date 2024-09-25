30f133b0402cf7c558058f6ddfb659859146c25d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2024-09-25 15:37:07 -0300

    Exercício 3 e adicionando comentários - Belton Lee Carr de Muzio Meira

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 // Exercício 1
 
-let variavelNulo = null; //Variável do tipo nulo.
-let variavelUndefined; //Variável do tipo undefined.
-let variavel = 10; //Variável com valor
+// let variavelNulo = null; //Variável do tipo nulo.
+// let variavelUndefined; //Variável do tipo undefined.
+// let variavel = 10; //Variável com valor
 
-// console.log(variavelNulo);
-// console.log(variavelUndefined);
-// console.log(variavel);
+// console.log(variavelNulo); // Print da variável nulo
+// console.log(variavelUndefined); // Print da variavel undefined
+// console.log(variavel); // Print da variavel com valor
 
 // Exercício 2
 
-let val = "12";
-let valFloat= parseFloat(val)
-console.log(typeof valFloat);
+// let val = "12"; // Variável
+// let valFloat= parseFloat(val) //Convertendo variável
+// console.log(typeof valFloat); //Print do tipo da variável
+
+// // Exercício 3
+// // Declar a variável como int
+// let numero = 10;
+// console.log(typeof numero);
+
+// // Converte a variável para float
+// numero = parseFloat(numero);
+// console.log(typeof numero);
+
+// Exercício 4
+
+// let numeroFloat = 12.45 // Variável float
+// console.log(typeof parseInt(numeroFloat)) // print do tipo da variável convertida para int
