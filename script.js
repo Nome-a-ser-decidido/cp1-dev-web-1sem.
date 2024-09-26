aa912fd84671abbb63ced1975c941c9bfe916e5d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2024-09-26 10:23:16 -0300

    Consertando o código

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // Exercício 1
 
-// let variavelNulo = null; //Variável do tipo nulo.
-// let variavelUndefined; //Variável do tipo undefined.
-// let variavel = 10; //Variável com valor
+let variavelNulo = null; //Variável do tipo nulo.
+let variavelUndefined; //Variável do tipo undefined.
+let variavel = 10; //Variável com valor
 
-// console.log(variavelNulo); // Print da variável nulo
-// console.log(variavelUndefined); // Print da variavel undefined
-// console.log(variavel); // Print da variavel com valor
+console.log(variavelNulo); // Print da variável nulo
+console.log(variavelUndefined); // Print da variavel undefined
+console.log(variavel); // Print da variavel com valor
 
 // Exercício 2
 
-// let val = "12"; // Variável
-// let valFloat= parseFloat(val) //Convertendo variável
-// console.log(typeof valFloat); //Print do tipo da variável
+let val = "12"; // Variável
+let valFloat= parseFloat(val) //Convertendo variável
+console.log(typeof valFloat); //Print do tipo da variável
 
 // // // Exercício 3
-// // Declar a variável como int
-// let numero = 10;
-// console.log(typeof numero);
+// Declar a variável como int
+let numero = 10;
+console.log(typeof numero);
 
-// // Converte a variável para float
-// numero = parseFloat(numero);
-// console.log(typeof numero);
+// Converte a variável para float
+numero = parseFloat(numero);
+console.log(typeof numero);
 
 // Exercício 4
 
-// let numeroFloat = 12.45 // Variável float
-// console.log(typeof parseInt(numeroFloat)) // print do tipo da variável convertida para int
+let numeroFloat = 12.45 // Variável float
+console.log(typeof parseInt(numeroFloat)) // print do tipo da variável convertida para int
 
 // Exercício 5
 let nome="Kaike Vital"; // Variável nome
